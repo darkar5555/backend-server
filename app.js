7dedb8519d124a39b882baa93cae3fb05b343434
darkar5555 <rodrigo.castro@ucsp.edu.pe>
2018-02-02 18:35:30 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+//Inicializar variables
+var app = express();
+
+
+//conexion a la bese de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>{
+    if (err) throw err;
+    console.log('Base de datos: \x1b[31m\x1b[0m', 'online');
+})
+
+
+//rutas
+app.get('/', (req, res, next)=>{
+    
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+});
+
+
+//Escuchar peticiones
+app.listen(3000, ()=>{
+    console.log('express server corriendo en el puerto 3000: \x1b[31m\x1b[0m', 'online');
+});
